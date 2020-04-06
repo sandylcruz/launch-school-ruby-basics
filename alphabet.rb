46b916263c5ca656354fa5bfcf182835e1d92b67
Sandy Cruz <sandyleahcruz@icloud.com>
2020-04-06 14:57:09 -0700

    Split alphabet letters

---
+++ alphabet.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Using the following code, split the value of alphabet
+# by individual characters and print each character.
+
+def split
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  print alphabet.split('')
+end
+puts split
