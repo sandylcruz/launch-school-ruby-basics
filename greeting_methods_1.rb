13c711ba643e0e6256b4752cce54688ba0cd6921
Sandy Cruz <sandyleahcruz@icloud.com>
2020-04-10 14:34:20 -0700

    Use string intepolation

---
+++ greeting_methods_1.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Write two methods, one that returns the string "Hello"
+# and one that returns the string "World". Then print
+# both strings using #puts, combining them into one sentence.
+
+def hello
+  puts 'Hello'
+end
+
+def world
+  puts 'World'
+end
+
+puts "#{hello} #{world}"
