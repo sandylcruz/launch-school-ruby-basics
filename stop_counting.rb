906e447a70a7d2c81193e6d910fbaf4c5e065c95
Sandy Cruz <sandyleahcruz@icloud.com>
2020-04-17 15:39:33 -0700

    Wrote program to keep counting until told to stop

---
+++ stop_counting.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# The method below counts from 0 to 4. Modify the block
+# so that it prints the current number and stops
+# iterating when the current number equals 2.
+
+def stop_counting
+  5.times do |index|
+    puts index
+    break if index == 2
+  end
+end
+
+puts stop_counting
