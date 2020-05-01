e1b294b34ae82e472e34f63c1cca004ae0194de3
Sandy Cruz <sandyleahcruz@icloud.com>
2020-05-01 12:47:04 -0700

    Print out only anagrams

---
+++ hashes_challenge.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Write a program that prints out groups of words
+# that are anagrams. Anagrams are words that have the
+# same exact letters in them but in a different order.
+
+def anagram
+  words = %w[demo none tied evil dome mode
+             live fowl veil wolf diet vile
+             edit tide flow neon]
+end
